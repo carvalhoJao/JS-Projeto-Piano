9a4d3492feee3f28850b137571ba2c595c5bb23d
carvalhoJao <pedrocarvalho2021@hotmail.com>
2024-01-22 22:03:06 -0300

    Adicionando os controles de som e tecla

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 let audio = new Audio("assets/tunes/a.wav");
 let mapedKeys = [];
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
+
+// Função principal
 const playTune = (key) => {
     audio.src = `assets/tunes/${key}.wav`;
     audio.play();
@@ -17,8 +21,24 @@ pianoKeys.forEach((key) => {
     mapedKeys.push(key.dataset.key);   
 });
 
+// Controle do teclado
 document.addEventListener("keydown", (e) => {
     if(mapedKeys.includes(e.key)){
         playTune(e.key);
     }
 });
+
+// Controle de Volume
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+
+const showHideKeys = () => {
+    pianoKeys.forEach((key) => {
+        key.classList.toggle("hide")
+    })
+}
+
+keysCheck.addEventListener("click", showHideKeys);
